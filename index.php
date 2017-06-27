422cb8fc8ba07852373497f77ae0c69fe29d756f
Ryoho Shinya <Ryoho Shinya>
2017-06-28 02:17:06 +0900

    とりあえずうごいた版

--- index.php
+++ index.php
@@ -5,17 +5,19 @@ use Symfony\Component\HttpFoundation\Request;
 
 date_default_timezone_set('Asia/Tokyo');
 
-// 環境変数の値を取得する
-$access_token = getenv('LINE_CHANNEL_ACCESS_TOKEN');
-$chat_api_key = getenv('DOCOMO_CHAT_API_KEY');
-$redis_url = getenv('REDIS_URL');
-
 $app = new Silex\Application();
 
 $app->post('/callback', function (Request $request) use ($app) {
+    // 環境変数の値を取得する
+    $access_token = getenv('LINE_CHANNEL_ACCESS_TOKEN');
+    $redis_url = getenv('REDIS_URL');
+    $bot_mode = getenv('BOT_MODE');
+
+    error_log($bot_mode);
 
     //ユーザーからのメッセージ取得する
     $contents = file_get_contents('php://input');
+    error_log($contents);
     $msg_obj = json_decode($contents);
 
     // Webhook Event Objectから必要なデータを取得する
@@ -31,38 +33,41 @@ $app->post('/callback', function (Request $request) use ($app) {
     }
 
     $reply_text = '';
-    if ($chat_api_key == '') {
+    if ($bot_mode == 'DOCOMO') {
+        // DOCOMOの雑談対話APIを使用するボット
+        $redis = new Predis\Client($redis_url);
+        $context = $redis->get($user_id);
+        $response = dialogue($msg_text, $context);
+        $redis->set($user_id, $response->context);
+        $reply_text = $response->utt;
+    } else {
         // パターンで返すシンプルなボット(4パターン程度)
         switch ($msg_text) {
-            case 'あそぼ'
+            case 'あそぼ':
                 $reply_text = '昼休みは部室で勉強って約束したやん？';
                 break;
-            case 'ウソ'
+            case 'ウソ':
                 $reply_text = 'ウソつくとワシワシするよ！';
                 break;
-            case 'カード'
+            case 'カード':
                 $reply_text = 'カードがウチにそう告げるんや！！！';
                 break;
             default:
                 $reply_text = 'やってみればええやん。特に理由なんて必要ない。やりたいからやってみる。本当にやりたいことって、そんな感じに始まるんやない？';
                 break;
         }
-    } else {
-        $redis = new Predis\Client($redis_url);
-        $context = $redis->get($user_id);
-        $response = dialogue($msg_text, $context);
-        $redis->set($user_id, $response->context);
-        $reply_text = $response->utt;
     }
+
+    error_log($reply_text);
+
+    $message = [
+        'type' => 'text',
+        'text' => $reply_text
+    ];
 
     $post_data = [
         'replyToken' => $reply_token,
-        'messages' => [
-            [
-                'type' => 'text',
-                'text' => $reply_text
-            ]
-        ]
+        'messages' => [$message]
     ];
 
     $ch = curl_init('https://api.line.me/v2/bot/message/reply');
@@ -70,12 +75,12 @@ $app->post('/callback', function (Request $request) use ($app) {
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json; charser=UTF-8',
-            'Authorization: Bearer ' . $access_token
-        )
-    );
-    curl_exec($ch);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json; charser=UTF-8',
+        'Authorization: Bearer ' . $access_token
+    ]);
+    $result = curl_exec($ch);
+    error_log($result);
     curl_close($ch);
 
     return 0;
@@ -90,13 +95,30 @@ $app->post('/callback', function (Request $request) use ($app) {
  * @link https://dev.smt.docomo.ne.jp/?p=docs.api.page&api_name=dialogue&p_name=api_1#tag01
  */
 function dialogue($message, $context) {
-    $post_data = array('utt' => $message, 'context' => $context);
-    // DOCOMOに送信
-    $ch = curl_init("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=". $chat_api_key);
+    // 環境変数の取得
+    $chat_api_key = getenv('DOCOMO_CHAT_API_KEY');
+
+    $post_data = [
+        'utt' => $message,
+        'context' => $context,
+        'nickname' => '希',
+        'nickname_y' => 'ノゾミ',
+        'sex' => '女',
+        'bloodtype' => 'O',
+        'birthdateM' => '6',
+        'birthdateD' => '9',
+        'age' => '17',
+        'constellations' => '双子座',
+        'place' => '東京',
+        't' => '20'
+    ];
+    $ch = curl_init('https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=' . $chat_api_key);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Content-Type: application/json; charser=UTF-8"
+        'Content-Type: application/json; charser=UTF-8'
     ]);
     $result = curl_exec($ch);
     curl_close($ch);
